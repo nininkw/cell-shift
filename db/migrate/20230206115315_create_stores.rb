38f4c6d9e90b6c2fd0642c6bd96ae20f21d053e9
nininkw <hraktai012ka@gmail.com>
2023-02-06 21:20:14 +0900

    add #7 storeテーブル追加、ルーティング設定

---
+++ db/migrate/20230206115315_create_stores.rb
@@ -0,0 +1,13 @@
+class CreateStores < ActiveRecord::Migration[6.1]
+  def change
+    create_table :stores do |t|
+      t.string :name
+      t.string :address
+      t.time :business_hour
+      t.string :near_stations
+      t.string :shift_pattern
+
+      t.timestamps
+    end
+  end
+end
