41d3eef18d11a4bdd1a02ab878cb5fb28d0a85c9
nininkw <hraktai012ka@gmail.com>
2023-02-07 18:55:21 +0900

    add #7 storeテーブル追加

--- db/migrate/20230206115315_create_stores.rb
+++ db/migrate/20230206115315_create_stores.rb
@@ -6,7 +6,7 @@ class CreateStores < ActiveRecord::Migration[6.1]
       t.time :business_hour
       t.string :near_stations
       t.string :shift_pattern
-
+      t.string :shift_schedule
       t.timestamps
     end
   end
