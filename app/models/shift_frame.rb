074ae075d9ca961c0faced1c713f28cbad977259
nininkw <hraktai012ka@gmail.com>
2024-03-20 19:03:22 +0900

    add #19 seedファイル編集。userとstoreの初期情報を追加

--- app/models/shift_frame.rb
+++ app/models/shift_frame.rb
@@ -1,4 +1,26 @@
 class ShiftFrame < ApplicationRecord
   belongs_to :store
 
+  validates :shift_name, inclusion: {in:0..3}
+  validate  :start_finish_check
+
+  def start_finish_check
+    errors.add(:start_at, "勤務開始時間より前の時間を登録できません。") unless
+    self.start_at < self.finish_at
+  end
+
+  def shift_name_text
+    case shift_name
+    when 0
+      "通し"
+    when 1
+      "早番"
+    when 2
+      "中番"
+    when 3
+      "遅番"
+    else
+      "不明な値"
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,126 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: "kazuki",
-             email: "kazuki@kazuki-mail.com",
+User.create!(name: "管理人",
+             email: "adadminmin@admin.com",
              password: "asdfghjkl",
              password_confirmation: "asdfghjkl", 
              admin: true
              )
+
+15.times do |n|
+  email = Faker::Internet.email
+  User.create!(
+    name: Faker::Name.name,
+    email: email, 
+    password: 'foobar',
+    password_confirmation: 'foobar',
+    shift_stabilize: rand(0..3), 
+    wday: (0..7).to_a.sample(rand(1..8)), 
+    max_work: rand(0..3), 
+    store_id: rand(1..4), 
+    wages: rand(1113..1400), 
+    holiday: [true, false].sample, 
+    priority: [true, false].sample 
+  )
+end
+
+#青山店
+store_aoyama = Store.create!(
+                            name: "青山店",
+                            open_at: "10:00",
+                            close_at: "21:00",
+                            need_workers: 2
+                            )
+ShiftFrame.create!(
+                    shift_name: 1,
+                    start_at: "9:30",
+                    finish_at: "18:30",
+                    store_id: store_aoyama.id
+                    )
+ShiftFrame.create!(
+                    shift_name: 3,
+                    start_at: "12:00",
+                    finish_at: "21:00",
+                    store_id: store_aoyama.id
+                    )
+
+#渋谷店
+store_shibuya = Store.create!(
+                              name: "渋谷店",
+                              open_at: "7:00",
+                              close_at: "23:00",
+                              need_workers:2
+                              )
+ShiftFrame.create!(
+                    shift_name: 1,
+                    start_at: "6:30",
+                    finish_at: "15:30",
+                    store_id: store_shibuya.id
+                    )
+ShiftFrame.create!(
+                    shift_name: 3,
+                    start_at: "14:00",
+                    finish_at: "23:00",
+                    store_id: store_shibuya.id
+                    )
+
+#宮下公園店
+store_park = Store.create!(
+                          name: "宮下公園店",
+                          open_at: "7:00",
+                          close_at: "23:30",
+                          need_workers: 3
+                          )
+ShiftFrame.create!(
+                    shift_name: 1,
+                    start_at: "6:30",
+                    finish_at: "15:30",
+                    store_id: store_park.id
+                    )
+ShiftFrame.create!(
+                    shift_name: 2,
+                    start_at: "10:00",
+                    finish_at: "19:00",
+                    store_id: store_park.id
+                    )
+ShiftFrame.create!(
+                    shift_name: 3,
+                    start_at: "14:00",
+                    finish_at: "23:00",
+                    store_id: store_park.id
+                    )
+  
+#神宮前店
+store_jingumae = Store.create!(
+                              name: "神宮前店",
+                              open_at: "11:00",
+                              close_at: "20:00",
+                              need_workers:1
+                              )
+ShiftFrame.create!(
+                    shift_name: 0,
+                    start_at: "10:30",
+                    finish_at: "20:00",
+                    store_id: store_jingumae.id
+                    )
+
+#シフト希望日を作成
+users = User.all
+
+start_date = Date.today
+end_date = 1.year.from_now
+
+users.each do |user|
+  (start_date..end_date).select {|d| d.day == 1}.each do |first_day_of_month|
+    last_day_of_month = first_day_of_month.end_of_month
+
+    days_of_month = rand(1..20).times.map do
+      rand(first_day_of_month..last_day_of_month)
+    end.uniq
+
+    days_of_month.each do |day|
+      ShiftPost.create!(user: user, date: day)
+    end
+  end
+end
